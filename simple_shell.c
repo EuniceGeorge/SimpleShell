e3205285d0e0c5502670dea3adc62e476b0e5f9b
Eseoghene-ODODO <en.ododo@gmail.com>
2023-10-10 02:04:02 +0000

    Update on task 0

--- simple_shell.c
+++ simple_shell.c
@@ -18,47 +18,41 @@
  */
 int main(void)
 {
-    char buffer[BUFFER_SIZE];
-    ssize_t read_chars;
-    char *token;
+	char buffer[BUFFER_SIZE];
+	ssize_t read_chars;
+	char *token;
 
-    while (1)
-    {
-        printf("$ ");  /* Display prompt */
+	while (1)
+	{
+		printf("$ ");  /* Display prompt */
+		read_chars = getline(&token, &read_chars, stdin);
+		if (read_chars == -1)
+		{
+			printf("\n");
+			free(token);
+			exit(EXIT_SUCCESS);  /* Check for end of file (Ctrl+D) */
+		}
+		token[strlen(token) - 1] = '\0';  /* Remove newline character */
+		pid_t child_pid = fork();  /* Fork to create a child process */
 
-        read_chars = getline(&token, &read_chars, stdin);  /* Read command from user */
-
-        if (read_chars == -1)
-        {
-            printf("\n");
-            free(token);
-            exit(EXIT_SUCCESS);  /* Check for end of file (Ctrl+D) */
-        }
-
-        token[strlen(token) - 1] = '\0';  /* Remove newline character */
-
-        pid_t child_pid = fork();  /* Fork to create a child process */
-
-        if (child_pid == -1)
-        {
-            perror("Error forking");
-            exit(EXIT_FAILURE);
-        }
-
-        if (child_pid == 0)  /* Child process */
-        {
-            if (execve(token, NULL, NULL) == -1)  /* Execute the command using execve */
-            {
-                fprintf(stderr, "%s: command not found\n", token);
-                free(token);
-                exit(EXIT_FAILURE);
-            }
-        }
-        else  /* Parent process */
-        {
-            wait(NULL);  /* Wait for the child to finish */
-        }
-    }
-
-    return 0;
+		if (child_pid == -1)
+		{
+			perror("Error forking");
+			exit(EXIT_FAILURE);
+		}
+		if (child_pid == 0)  /* Child process */
+		{
+			if (execve(token, NULL, NULL) == -1)  /* Execute the command using execve */
+			{
+				fprintf(stderr, "%s: command not found\n", token);
+				free(token);
+				exit(EXIT_FAILURE);
+			}
+		}
+		else  /* Parent process */
+		{
+			wait(NULL);  /* Wait for the child to finish */
+		}
+	}
+	return (0);
 }
