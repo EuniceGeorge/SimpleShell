4e9906d5462489c8d3c8f9f6850c2ac6c7330555
Eseoghene-ODODO <en.ododo@gmail.com>
2023-10-19 16:03:36 +0000

    I added shell_exit file

---
+++ shell_exit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * shell_exit - Built-in exit command with status handling
+ * @args: The arguments for the exit command
+ */
+void shell_exit(char *args[])
+{
+	int status = 0;
+
+	if (args[1] != NULL)
+	{
+		status = atoi(args[1]);
+
+		if (status == 0 && args[1][0] != '0')
+		{
+			fprintf(stderr, "shell: exit: %s: numeric argument required\n",
+					args[1]);
+			exit(EXIT_FAILURE);
+		}
+	}
+	exit(status);
+}
